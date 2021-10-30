241c2ef8f4f71105ff8e0655a1cf7d6f24354428
ga1kokuj1n <maksimp.20040903@gmail.com>
2021-10-30 18:44:27 +0300

    Lab2.1 solved

--- Lab2.1/Lab2.1.cs
+++ Lab2.1/Lab2.1.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Lab2._1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Решение ax^2 + bx + c = 0");
+			Console.Write("Ввдеите a: ");
+			double a = double.Parse(Console.ReadLine());
+			Console.Write("Введите b: ");
+			double b = double.Parse(Console.ReadLine());
+			Console.Write("Введите c: ");
+			double c = double.Parse(Console.ReadLine());
+
+			double disc = b * b - 4 * a * c;
+			if (disc == 0)
+			{
+				double x = -b / (2 * a);
+				Console.WriteLine($"2 совпадающих действительных корня:\nx1 = x2 = {x}");
+			}
+			else if (disc > 0)
+			{
+				double x1 = (-b + Math.Sqrt(disc)) / (2 * a);
+				double x2 = (-b - Math.Sqrt(disc)) / (2 * a);
+				Console.WriteLine($"2 действительных корня:\nx1 = {x1}\nx2 = {x2}");
+			}
+			else
+			{
+				double real = -b / (2 * a);
+				double imag = Math.Sqrt(-disc);
+                Console.WriteLine($"2 комплексных корня:\nx1 = {real} + {imag}i\nx2 = {real} - {imag}i");
+			}
+		}
+	}
 }
